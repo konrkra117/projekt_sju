0e83a1e32354ac007513511b92db8666fa1fd53c
konrkra117 <72416870+konrkra117@users.noreply.github.com>
2025-05-24 17:03:50 +0200

    ehhh

--- test.py
+++ test.py
@@ -6,14 +6,14 @@ def test_imports():
         "Cryptodome",  # Pycryptodomex
         "cryptography"
     ]
-
-for pkg in packages:
-    try:
-        __import__(pkg)
-        print(f"✅ {pkg} - OK")
-    except ImportError:
-        print(f"❌ {pkg} - MISSING")
-        exit(1)
+    
+    for pkg in packages:
+        try:
+            __import__(pkg)
+            print(f"✅ {pkg} - OK")
+        except ImportError:
+            print(f"❌ {pkg} - MISSING")
+            exit(1)
 
 if __name__ == "__main__":
     test_imports()
